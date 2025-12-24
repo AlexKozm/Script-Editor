f41064fef418b913fe9b383d6d703941e1671c21
Aleksandr Kozminykh <sa.kozminykh@gmail.com>
2025-12-24 11:54:38 +0100

    init

---
+++ composeApp/src/jvmMain/kotlin/com/example/scripteditor/main.kt
@@ -0,0 +1,13 @@
+package com.example.scripteditor
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "scripteditor",
+    ) {
+        App()
+    }
+}
