4e461cd6996194b4499e808e4185b5d883032669
Aleksandr Kozminykh <sa.kozminykh@gmail.com>
2026-01-23 21:48:53 +0100

    new interface
    but not good implementation multiple collection from scriptOutput brakes executionState

--- core/src/commonMain/kotlin/com/example/scripteditor/domain/ScriptService.kt
+++ core/src/commonMain/kotlin/com/example/scripteditor/domain/ScriptService.kt
@@ -2,15 +2,26 @@ package com.example.scripteditor.domain
 
 import com.example.scripteditor.core.ExecutionEvent
 import com.example.scripteditor.core.ExecutionState
-
-import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 
 
 interface ScriptService {
-    val scriptOutput: SharedFlow<ExecutionEvent>
+    /**
+     * Flow is empty until [runScript] is called.
+     * Script cancels as soon as collector's coroutine scope cancels or [stopScript] is called.
+     */
+    val scriptOutput: Flow<ExecutionEvent>
+
     val executionState: StateFlow<ExecutionState>
 
+    /**
+     * Works only if [scriptOutput] is collected. Overwise does nothing
+     */
     suspend fun runScript(command: String, arguments: List<String>)
+
+    /**
+     * Works only if [scriptOutput] is collected. Overwise does nothing
+     */
     suspend fun stopScript()
 }
--- core/src/commonMain/kotlin/com/example/scripteditor/domain/ScriptServiceImpl.kt
+++ core/src/commonMain/kotlin/com/example/scripteditor/domain/ScriptServiceImpl.kt
@@ -9,27 +9,28 @@ import kotlinx.coroutines.flow.*
 class ScriptServiceImpl(
     private val scriptExecutionRepository: ScriptExecutionRepository = ScriptExecutionRepository(),
 ) : ScriptService {
-
-    private val _scriptOutput: MutableSharedFlow<ExecutionEvent> = MutableSharedFlow()
-    override val scriptOutput: SharedFlow<ExecutionEvent> = _scriptOutput.asSharedFlow()
-
     private val _executionState: MutableStateFlow<ExecutionState> = MutableStateFlow(ExecutionState.STOPPED)
     override val executionState: StateFlow<ExecutionState> = _executionState
 
-    private var executionJob: Job? = null
+    private val executionRequestFlow: MutableSharedFlow<ScriptParams?> = MutableSharedFlow()
 
-    override suspend fun runScript(command: String, arguments: List<String>): Unit = coroutineScope {
-        stopScript()
-        executionJob = launch(Dispatchers.Default) {
-            _executionState.value = ExecutionState.RUNNING
-            scriptExecutionRepository.run(command, arguments).collect(_scriptOutput::emit)
+    override val scriptOutput: Flow<ExecutionEvent> = flow {
+        executionRequestFlow.collectLatest { request ->
+            _executionState.value = request?.let { ExecutionState.RUNNING } ?: ExecutionState.STOPPED
+            request?.run { scriptExecutionRepository.run(command, arguments).collect(::emit) }
         }
-        executionJob?.invokeOnCompletion { _executionState.value = ExecutionState.STOPPED }
     }
 
-    override suspend fun stopScript() = coroutineScope {
+    override suspend fun runScript(command: String, arguments: List<String>) =
+        executionRequestFlow.emit(ScriptParams(command, arguments))
+
+    override suspend fun stopScript() {
         _executionState.value = ExecutionState.STOPPING
-        executionJob?.cancelAndJoin()
-        executionJob = null
+        executionRequestFlow.emit(null)
     }
 }
+
+private data class ScriptParams(
+    val command: String,
+    val arguments: List<String>
+)
